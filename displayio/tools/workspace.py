5e9f04f56f54898b015ce17a580822257324f501
Robert Grizzell <robert@grizzell.me>
2023-09-20 23:48:09 -0500

    Fixing up the workspace to use the full screen

--- displayio/tools/workspace.py
+++ displayio/tools/workspace.py
@@ -1,89 +1,65 @@
+""" Workspace used for display testing on an ST7789 """
 import board
 import busio
+import displayio
 from displayio import Group, FourWire, release_displays
-import adafruit_st7789
+import time
 
+import adafruit_st7789
 from adafruit_display_shapes.rect import Rect
 from adafruit_display_shapes.circle import Circle
 
-_WIDTH = 320
-_HEIGHT = 240
 
-_FILL = 0x000000
-_OUTLINE = 0xFFFFFF
-
-screen = Group()
-
-
-def init_display():
-    # Define pins connected to the chip
-    tft_sck = board.GP18
-    tft_mosi = board.GP19
-    tft_blk = board.GP20
-    tft_dc = board.GP16
-    tft_cs = board.GP21
-    tft_rst = None
-
-    # Reset the chip and re-initialize it
+def init_display(width: int = 320, height: int = 240, rotation: int = 0) -> displayio.Display:
     release_displays()
-    tft_spi = busio.SPI(tft_sck, tft_mosi)
-    tft_bus = FourWire(
-        spi_bus=tft_spi,
-        command=tft_dc,
-        chip_select=tft_cs,
-        reset=tft_rst
+    spi = busio.SPI(board.GP18, board.GP19)
+    bus = FourWire(
+        spi_bus=spi, command=board.GP16, chip_select=board.GP21, reset=None
     )
     display = adafruit_st7789.ST7789(
-        bus=tft_bus,
-        backlight_pin=tft_blk,
-        rotation=270,
-        width=_WIDTH,
-        height=_HEIGHT
+        bus=bus,
+        backlight_pin=board.GP20,
+        rotation=rotation,
+        width=width,
+        height=height,
     )
 
-    screen = Group()
-    display.show(screen)
-    return screen
+    return display
 
 
-def build_workspace():
+def workspace(width, height, fill, outline):
     border = Rect(
-        x=0,
-        y=0,
-        width=200,
-        height=200,
-        fill=_FILL,
-        outline=_OUTLINE,
-        stroke=1
-    )
-    calibration_square = Rect(
-        x=0,
-        y=0,
-        width=20,
-        height=20,
-        fill=_FILL,
-        outline=_OUTLINE,
-        stroke=1
+        x=0, y=0, width=width, height=height, fill=fill, outline=outline, stroke=1
     )
     center_circle = Circle(
-        x0=100,
-        y0=100,
-        r=10,
-        fill=_FILL,
-        outline=_OUTLINE,
-        stroke=1
+        x0=width // 2, y0=height // 2, r=10, fill=fill, outline=outline, stroke=1
+    )
+    nw_square = Rect(
+        x=0, y=0, width=20, height=20, fill=fill, outline=outline, stroke=1
+    )
+    ne_square = Rect(
+        x=300, y=0, width=20, height=20, fill=fill, outline=outline, stroke=1
+    )
+    sw_square = Rect(
+        x=0, y=220, width=20, height=20, fill=fill, outline=outline, stroke=1
+    )
+    se_square = Rect(
+        x=300, y=220, width=20, height=20, fill=fill, outline=outline, stroke=1
     )
 
-    workspace = Group()
-    workspace.append(border)
-    workspace.append(calibration_square)
-    workspace.append(center_circle)
-    return workspace
+    g = Group()
+    g.append(border)
+    g.append(nw_square)
+    g.append(ne_square)
+    g.append(sw_square)
+    g.append(se_square)
+    g.append(center_circle)
+    return g
 
 
 if __name__ == "__main__":
-    screen = init_display()
-    screen.append(build_workspace())
+    display = init_display(rotation=270)
+    display.show(workspace(320, 240, 0x000000, 0xFFFFFF))
 
     while True:
-        time.sleep(1)
+        time.sleep(5)
